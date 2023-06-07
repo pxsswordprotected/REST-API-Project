1538c5262876b5896433c7a79b56029b10d5bc97
pxsswordprotected <jamesalford661@gmail.com>
2023-06-07 12:19:37 -0500

    helper for authentication

---
+++ src/helpers/index.ts
@@ -0,0 +1,11 @@
+//use authentication helpers to encrypt the password or create random tokens
+
+import crypto from 'crypto';
+
+const SECRET= 'SHANKS-REST-API';
+
+export const random = () => crypto.randomBytes(128).toString('base64'); // generated string will be a sequence of random characters encoded in Base64 format
+
+export const authentication = (salt: string, password: string) => { //perform authentication using a password and salt. It creates an HMAC object with the SHA-256 algorithm, updates the hash with the secret value, and generates the resulting hash in a hexadecimal format
+    return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest('hex');
+}
