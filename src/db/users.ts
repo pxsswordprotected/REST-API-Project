08ccad03c614b2133dd9270eea1e18f88b41b261
pxsswordprotected <jamesalford661@gmail.com>
2023-06-07 12:19:03 -0500

    helpers for authentication

--- src/db/users.ts
+++ src/db/users.ts
@@ -16,13 +16,14 @@ export const UserModel = mongoose.model('User', UserSchema); //exports a Mongoos
 export const getUsers = () => UserModel.find();
 export const getUserByEmail = (email: string) => UserModel.findOne({ email });
 
-export const getUserBySessionToken   = (sessionToken: string)   => UserModel.findOne({ //confirm whether our user is logged in or not
+export const getUserBySessionToken = (sessionToken: string)   => UserModel.findOne({ //confirm whether our user is logged in or not
     'authentication.sessionToken': sessionToken,
 });
-export  const getUserById = (id: string) => UserModel.findById(id);
+export  const getUserById = (id: string) => UserModel.findById(id); //retrieves a user from the database based on their ID
 
-export const createUser = (values: Record<string, any>) => new UserModel(values)
+export const createUser = (values: Record<string, any>) => new UserModel(values) //create a new user in the database
  .save().then((user) => user.toObject());
+ 
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id}); //delete a user from the database based on their ID. It searches for a user with the matching ID and removes the corresponding document from the collection.
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values); //update a user in the database based on their ID. It searches for a user with the matching ID, modifies the user document with the provided values, and saves the updated document to the database.
 
-export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id});
-export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
