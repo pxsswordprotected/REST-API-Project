c3681aa723aba6065979629e2f1505de950c2eb3
pxsswordprotected <jamesalford661@gmail.com>
2023-06-07 15:40:47 -0500

    get user

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import authentication from './authentication';
+import users from './users';
 
 const router = express.Router(); //creates a new router instance
 
 export default (): express.Router => { // returns an Express router
     authentication(router);
+    users(router);
     
     return router;
 };
