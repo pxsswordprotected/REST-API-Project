0b4ac030c6895fdae3f2fdf6b423ad9a7414c406
pxsswordprotected <jamesalford661@gmail.com>
2023-06-07 17:27:02 -0500

    fixed the 404 bug in postman

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+
+import router from './router';
 import mongoose from 'mongoose';
-import router from 'router';
 
 const app = express();
 
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,6 +3,33 @@ import { get, merge } from 'lodash';
 
 import { getUserBySessionToken } from '../db/users';
 
+export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try { 
+
+   // ensure that the session token is valid and corresponds to an existing user. 
+
+    const sessionToken = req.cookies['SHANKS-AUTH']; // retrieves the value of the session token from the cookie named 'SHANKS-AUTH' that was sent with the request
+
+    if (!sessionToken) {
+      return res.sendStatus(403);
+    }
+
+    const existingUser = await getUserBySessionToken(sessionToken);
+
+  if(!existingUser) {
+      return res.sendStatus(403);
+  }
+
+  merge(req, { identity: existingUser }); // adding the existingUser information to the req object, specifically under the identity property
+
+  return next();
+
+  } catch (error) {
+      console.log(error);
+      return res.sendStatus(400);
+  }
+}
+
 export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   
   // used to check if the current user is the owner of a resource based on their identity and the id parameter
@@ -28,29 +55,3 @@ export const isOwner = async (req: express.Request, res: express.Response, next:
   }
 }
 
-export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    try { 
-
-     // ensure that the session token is valid and corresponds to an existing user. 
-
-      const sessionToken = req.cookies['SHANKS-AUTH']; // retrieves the value of the session token from the cookie named 'SHANKS-AUTH' that was sent with the request
-
-      if (!sessionToken) {
-        return res.sendStatus(400);
-      }
-
-      const existingUser = await getUserBySessionToken(sessionToken);
-
-    if(!existingUser) {
-        return res.sendStatus(400);
-    }
-
-    merge(req, { identity: existingUser }); // adding the existingUser information to the req object, specifically under the identity property
-
-    return next();
-
-    } catch (error) {
-        console.log(error);
-        return res.sendStatus(400);
-    }
-}
